51cee2744804f9ad16e12d11f7bab6aecb4aeb78
Raymond Jang <nicholasko@Nicholass-MacBook-Pro.local>
2021-12-10 16:43:35 -0800

    finished up sign in page and updated sign in input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 /**const HatsPage = () => (
   <div>
@@ -12,10 +14,13 @@ import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
+    //header always on top of everything
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
