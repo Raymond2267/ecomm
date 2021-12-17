db029e26469f08f66dc106ac66d2ff2f740524d9
Raymond Jang <nicholasko@Nicholass-MacBook-Pro.local>
2021-12-17 13:35:13 -0800

    added redux functionality into app

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-
 import CustomButton from '../custom-button/custom-button.component';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  state = {
-    email: '',
-    password: '',
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
 
@@ -41,13 +47,17 @@ class SignIn extends React.Component {
           <FormInput
             name='password'
             type='password'
-            value={this.state.email}
+            value={this.state.password}
             handleChange={this.handleChange}
-            label='email'
+            label='password'
             required
           />
-
-          <CustomButton type='submit'> Sign In </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+//can be used in object but array has more usability
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//store: object holding state of whole app
+//can only change through dispatch
+
+export default store;
